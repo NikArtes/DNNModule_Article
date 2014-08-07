10bd4f1c164f6055282bd18bfeddce66c59bda9a
NikArtes <nikas.artes@gmail.com>
2014-08-07 15:14:31 +0600

    представления модуля в режиме просмотра статьи

---
+++ DNNModule/Controls/ArticleView.ascx
@@ -0,0 +1,48 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="ArticleView.ascx.cs" Inherits="Christoc.Modules.DNNModule.Controls.ArticleView" %>
+<fieldset>
+    <div class="dnnFormItem">
+<asp:Panel ID="plArticleTitle" Style="color:brown; font-family: 'Times New Roman'; font-size: 18pt" runat="server" CssClass="StyleModule">
+</asp:Panel>
+<asp:Panel ID="plArticleBody" Style="color: black; font-family: 'Times New Roman'; font-size: 14pt; margin-left: 15px" runat="server" CssClass="Normal ArticleBody">
+</asp:Panel>
+<div class="ArticleAdmin" runat="server" id="ArticleAdmin">
+    <asp:LinkButton ID="lnkEdit" runat="server" ResourceKey="lnkEdit" OnClick="lnkEdit_Click"></asp:LinkButton>
+    <asp:LinkButton ID="lnkDelete" runat="server" ResourceKey="lnkDelete" OnClick="lnkDelete_Click"></asp:LinkButton>
+    <br />
+    <br />
+    
+
+    <asp:Panel ID="Panel1" runat="server">
+        <asp:Repeater ID="rptTegsOnArticle" OnItemCommand="rptTegsOnArticle_ItemCommand" runat="server">
+            <ItemTemplate>
+                <asp:LinkButton ID="lnkTeg" runat="server"  Text='<%# HttpUtility.HtmlEncode(DataBinder.Eval(Container.DataItem, "NameTeg")) %>'></asp:LinkButton>
+            </ItemTemplate>
+        </asp:Repeater>
+    </asp:Panel>
+<br />
+<asp:TextBox ID="TextBoxComment" runat="server" Height="18px" Width="492px"></asp:TextBox>
+<asp:Button ID="ButtonEditComment" resourcekey="ButtonEditComment" runat="server" Height="35px" style="margin-top: 0px; margin-bottom: 12px" OnClick="ButtonEditComment_Click" />
+</div>
+
+
+
+<p>
+    &nbsp;<asp:Repeater ID="rptArticleComments" runat="server" OnItemDataBound="rptArticleComments_ItemDataBound">
+        <ItemTemplate>
+            <asp:Panel ID="pnlCommentImage" runat="server" Style="color: black; font-family: 'Times New Roman'; font-size: 14pt">
+                <asp:Image ID="ImageComment" ImageUrl='<%# DataBinder.Eval(Container.DataItem, "ImgNameUrl") %>' runat="server" Height="40px" style="margin-top: 0px" Width="40px" />
+                <%# HttpUtility.HtmlEncode(DataBinder.Eval(Container.DataItem, "NameAutor").ToString()) %>
+            </asp:Panel>
+            <asp:panel ID="pnlComment" runat="server"  Style="color: black; font-family: 'Times New Roman'; font-size: 14pt">
+                <%# HttpUtility.HtmlEncode(DataBinder.Eval(Container.DataItem, "BodyComment").ToString()) %>
+            </asp:panel> 
+        </ItemTemplate>
+    </asp:Repeater>
+</p>
+        </div>
+    </fieldset>
+
+
+
+
+
---
+++ DNNModule/Controls/ArticleView.ascx.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Web.UI;
+using DotNetNuke.Common;
+using DotNetNuke.Framework;
+using DotNetNuke.Entities.Modules;
+using DotNetNuke.Entities.Content.Common;
+using Christoc.Modules.DNNModule.Components;
+using System.Web.UI.WebControls;
+using System.Web;
+
+namespace Christoc.Modules.DNNModule.Controls
+{
+    public partial class ArticleView : DNNModuleModuleBase
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (ArticleId > 0)
+            {
+                var curArticle = ArticleController.GetArticle(ArticleId);
+                //display article info on the view control
+
+                plArticleTitle.Controls.Add(new LiteralControl(curArticle.Title));
+                plArticleBody.Controls.Add(new LiteralControl(Server.HtmlDecode(curArticle.Body)));
+
+                //change the page title, description, and add categories to keywords
+
+                var tp = (CDefault)Page;
+                tp.Title = curArticle.Title;
+                tp.Description = curArticle.Description;
+                lnkEdit.Attributes["href"] = EditUrl(String.Empty, String.Empty, "Edit", "aid=" + curArticle.ArticleId);
+
+                if (!IsEditable && UserId==-1)
+                    ArticleAdmin.Visible = false;
+
+                rptArticleComments.DataSource = ArticleController.GetComments(ArticleId, ModuleId);
+                rptArticleComments.DataBind();
+
+                rptTegsOnArticle.DataSource = ArticleController.GetTegsOnArticle(ArticleId);
+                rptTegsOnArticle.DataBind();
+
+            }   
+        }
+
+        protected void lnkEdit_Click(object sender, EventArgs e)
+        {
+            if (IsEditable)
+            {
+                var s = EditUrl(string.Empty, string.Empty, "Edit", "aid" + ArticleId);
+                Response.Redirect(s);
+            }
+        }
+
+        protected void lnkDelete_Click(object sender, EventArgs e)
+        {
+            if (IsEditable)
+            {
+                ArticleController.DeleteArticle(ArticleId);
+                Response.Redirect(Globals.NavigateURL());
+            }
+        }
+
+        protected void rptArticleComments_ItemDataBound(object sender, System.Web.UI.WebControls.RepeaterItemEventArgs e)
+        {
+            var pnlOtherControls = e.Item.FindControl("pnlOtherControls") as Panel;
+        }
+
+        protected void ButtonEditComment_Click(object sender, EventArgs e)
+        {
+            if (TextBoxComment.Text != null)
+            {
+                Comment c=new Comment();
+                c.ArticleId = ArticleId;
+                c.BodyComment = TextBoxComment.Text;
+                c.CreatedOnDate = DateTime.Now;
+                c.ModuleId = ModuleId;
+                c.UserId = UserId;
+                ArticleController.SaveComment(c);
+            }
+        }
+
+        protected void rptTegsOnArticle_ItemCommand(object source, RepeaterCommandEventArgs e)
+        {
+            LinkButton buf = (LinkButton)e.CommandSource;
+            string buf1 = buf.Text;
+            var s = Globals.NavigateURL() + "?Teg=" + HttpUtility.UrlEncode(buf1);
+            Response.Redirect(s);
+        }
+    }
+}
