130b455ac08c89988e1132d56dda272f922b4821
NikArtes <nikas.artes@gmail.com>
2014-08-07 15:31:14 +0600

    базовый класс модуля

---
+++ DNNModule/DNNModuleModuleBase.cs
@@ -0,0 +1,49 @@
+﻿/*
+' Copyright (c) 2014  Christoc.com
+'  All rights reserved.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+' TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+' THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+' CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+' DEALINGS IN THE SOFTWARE.
+' 
+*/
+using System;
+using System.Web;
+using DotNetNuke.Entities.Modules;
+using DotNetNuke;
+
+namespace Christoc.Modules.DNNModule
+{
+    public class DNNModuleModuleBase : PortalModuleBase
+    {
+        public int ArticleId
+        {
+            get
+            {
+                var qs = Request.QueryString["aid"];
+                if (qs != null)
+                    return Convert.ToInt32(qs);
+                return -1;
+            }
+        }
+
+        public string Teg
+        {
+            get 
+            {
+                var qs = Convert.ToString(Request.QueryString["Teg"]);
+                string sd = HttpUtility.UrlDecode(qs);
+                return Convert.ToString(qs);
+            }
+        }
+
+        
+
+        public string GetArticleLink(string articleId)
+        {
+            return DotNetNuke.Common.Globals.NavigateURL(TabId, String.Empty, "aid=" + articleId);
+        }
+    }
+}
