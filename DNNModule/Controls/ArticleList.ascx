d16ddc8cc43c2b4706620ba158ef194f434549c8
NikArtes <nikas.artes@gmail.com>
2014-08-07 15:12:24 +0600

    представления модуля в режиме вывода списка статей

---
+++ DNNModule/Controls/ArticleList.ascx
@@ -0,0 +1,67 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="ArticleList.ascx.cs" Inherits="Christoc.Modules.DNNModule.Controls.ArticleList" %>
+<%@ Register TagPrefix="dnn" Assembly="DotNetNuke.Web" Namespace="DotNetNuke.Web.UI.WebControls" %>
+
+<fieldset>
+    <div class="dnnFormItem">
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+&nbsp;<table class="auto-style1">
+    <tr>
+        <td class="auto-style3" colspan="2">
+            <asp:Label ID="Label1" resourcekey="Label1" runat="server" Style="font-family:'Times New Roman'; color:black; font-size:14pt"></asp:Label>
+        </td>
+    </tr>
+    <tr>
+        <td class="auto-style2" colspan="2">
+            <asp:Button ID="Button2" resourcekey="button2" runat="server" OnClick="Button2_Click" />
+            <asp:Button ID="Button3" resourcekey="button3" runat="server" OnClick="Button3_Click" />
+            <asp:Button ID="Button4" resourcekey="button4" runat="server" OnClick="Button4_Click" />
+            <asp:Button ID="Button5" resourcekey="button5" runat="server" OnClick="Button5_Click" />
+        </td>
+    </tr>
+    <tr>
+        <td class="auto-style5"><asp:Calendar ID="Calendar1" runat="server" OnSelectionChanged="Calendar1_SelectionChanged" ></asp:Calendar>
+        </td>
+        <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;
+            <asp:Image ID="Image1" ImageUrl="~/DesktopModules/DNNModule/Sourse/search.png" runat="server" Height="26px" style="margin-top: 0px" Width="25px" />
+&nbsp;&nbsp;
+<asp:TextBox ID="TextBox" runat="server" Width="216px" Height="16px" style="margin-top: 4px; margin-bottom: 8px;"></asp:TextBox>
+<asp:Button ID="Button1" runat="server" OnClick="Button1_Click" resourcekey="Button1" Font-Size="Small" Height="39px" Width="86px"/>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        </td>
+    </tr>
+    <tr>
+        <td class="auto-style2" colspan="2">
+<asp:Repeater ID="rptArticleList" runat="server" OnItemCommand="RptArticleListOnItemCommand" OnItemDataBound="RptArticleListOnItemDataBound">
+    <ItemTemplate>
+        <asp:Panel ID="Panel1" CssClass="ArticleWrapper" runat="server">
+            <asp:Panel runat="server" ID="pnlArticleTitle" Style="font-family: 'Times New Roman';  font-size: 18pt; margin-top: 10px" CssClass="HTitle">
+                <asp:HyperLink ID="lnkArticle" runat="server"  CssClass="HTitle" NavigateUrl='<%# GetArticleLink(DataBinder.Eval(Container.DataItem,"ArticleId").ToString())%>'><%# System.Web.HttpUtility.HtmlEncode(DataBinder.Eval(Container.DataItem,"Title").ToString()) %></asp:HyperLink>
+            </asp:Panel>
+            <asp:Panel runat="server" ID="pnlArticleDescription" Style="color: black; font-family: 'Times New Roman'; font-size: 14pt; margin-left: 10px" CssClass="Normal ArticleDescription">
+                <%# HttpUtility.HtmlDecode(DataBinder.Eval(Container.DataItem, "Description").ToString())%>
+            </asp:Panel>
+            <asp:Panel runat="server" ID="pnlAdminControls" CssClass="Normal ArticleAdmin" Visible="false">
+                <asp:LinkButton ID="lnkEdit" runat="server" ResourceKey="EditArticle.Text" CommandName="Edit"
+                    Visible="false" Enabled="false" />
+                <asp:LinkButton ID="lnkDelete" runat="server" ResourceKey="DeleteArticle.Text" CommandName="Delete"
+                    Visible="false" Enabled="false" />
+            </asp:Panel>
+        </asp:Panel>
+    </ItemTemplate>
+</asp:Repeater>
+            <br />
+<asp:Panel ID="pnlPaging" runat="server">
+    <asp:HyperLink ID="lnkPrevious" runat="server" resourcekey="lnkPrevious" Visible="false"
+        CssClass="lnkPrevious"></asp:HyperLink>
+    <asp:HyperLink ID="lnkNext" runat="server" resourcekey="lnkNext" Visible="false"
+        CssClass="lnkNext"></asp:HyperLink>
+</asp:Panel>
+
+        </td>
+    </tr>
+</table>
+&nbsp;
+        </div>
+    </fieldset>
+
+
---
+++ DNNModule/Controls/ArticleList.ascx.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Data;
+using System.Web.UI;
+using DotNetNuke.Entities.Content.Common;
+using DotNetNuke.Entities.Modules;
+using Christoc.Modules.DNNModule.Components;
+using System.Collections.Specialized;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Web.UI.WebControls;
+using DotNetNuke.Services.Exceptions;
+using DotNetNuke.Services.Localization;
+using DotNetNuke.UI.Utilities;
+using Globals = DotNetNuke.Common.Globals;
+
+namespace Christoc.Modules.DNNModule.Controls
+{
+    public partial class ArticleList : DNNModuleModuleBase
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Page.IsPostBack)
+                {
+                    if (ViewState["DateCount"] != null)
+                    {
+                        rptArticleList.DataSource = ArticleController.GeneralGetArticlesOfDate(ModuleId, (DateTime)ViewState["DateCount"]);
+                        rptArticleList.DataBind();
+                        ViewState["DateCount"] = null;
+                    }
+                    if (ViewState["TitleCount"] != null)
+                    {
+                        rptArticleList.DataSource = ArticleController.GeneralGetArticlesOfTitle(ModuleId, (string)ViewState["TitleCount"]);
+                        rptArticleList.DataBind();
+                        ViewState["TitleCount"] = null;
+                    }
+                    if (ViewState["DateCountDay"] != null)
+                    {
+                        rptArticleList.DataSource = ArticleController.GeneralGetArticlesOfDay(ModuleId, (DateTime)ViewState["DateCountDay"]);
+                        rptArticleList.DataBind();
+                        ViewState["DateCountDay"] = null;
+                    }
+                }
+                else
+                {
+                    if (Teg != null)
+                    {
+                        rptArticleList.DataSource = ArticleController.GeneralGetArticleOfTeg(Teg);
+                        rptArticleList.DataBind();
+                    }
+                    else 
+                    {
+                        rptArticleList.DataSource = ArticleController.GeneralGetArticlesOfTitle(ModuleId, "");
+                        rptArticleList.DataBind();
+                    }
+                }
+            }
+            catch (Exception exc)
+            {
+                Exceptions.ProcessModuleLoadException(this, exc);
+            }
+        }
+
+        private static void SetPagingLink(NameValueCollection queryString
+            , HyperLink link, bool showLink, int linkedPageId, int tabId)
+        {
+            if (showLink)
+            {
+                link.Visible = true;
+                queryString = new NameValueCollection(queryString);
+                queryString["p"] = linkedPageId.ToString(CultureInfo.InvariantCulture);
+                var additionalParameters = new List<string>(queryString.Count);
+
+                for (int i = 0; i < queryString.Count; i++)
+                {
+                    if (string.Equals(queryString.GetKey(i), "TABID", StringComparison.OrdinalIgnoreCase))
+                    {
+                        int newTabId;
+                        if (int.TryParse(queryString[i], NumberStyles.Integer, CultureInfo.InvariantCulture, out newTabId))
+                        {
+                            tabId = newTabId;
+                        }
+                    }
+                    else if (!string.Equals(queryString.GetKey(i), "LANGUAGE", StringComparison.OrdinalIgnoreCase))
+                    {
+                        additionalParameters.Add(queryString.GetKey(i) + "=" + queryString[i]);
+                    }
+                }
+
+                link.NavigateUrl = Globals.NavigateURL(tabId, string.Empty, additionalParameters.ToArray());
+            }
+            else
+            {
+                link.Visible = false;
+            }
+        }
+
+        protected void RptArticleListOnItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            //configure the Tags
+            var lnkDelete = e.Item.FindControl("lnkDelete") as LinkButton;
+            var lnkEdit = e.Item.FindControl("lnkEdit") as LinkButton;
+            var pnlAdminControls = e.Item.FindControl("pnlAdminControls") as Panel;
+            var pnlOtherControls = e.Item.FindControl("pnlOtherControls") as Panel;
+
+            var curArticle = (Article)e.Item.DataItem;
+
+            
+            
+            if (IsEditable && lnkDelete != null && lnkEdit != null)
+            {
+                pnlAdminControls.Visible = true;
+                lnkDelete.Visible = lnkDelete.Enabled = lnkEdit.Visible = lnkEdit.Enabled = true;
+                ClientAPI.AddButtonConfirm(lnkDelete, Localization.GetString("ConfirmDelete", LocalResourceFile));
+                lnkEdit.Attributes["href"] = EditUrl(String.Empty, String.Empty, "Edit", "aid=" + curArticle.ArticleId);
+                lnkDelete.CommandArgument = curArticle.ArticleId.ToString();
+                lnkEdit.CommandArgument = curArticle.ArticleId.ToString();
+            }
+            else
+            {
+                pnlAdminControls.Visible = false;
+            }
+        }
+
+        public void RptArticleListOnItemCommand(object source, RepeaterCommandEventArgs e)
+        {
+            if (e.CommandName == "Delete")
+            {
+                ArticleController.DeleteArticle(Convert.ToInt32(e.CommandArgument));
+            }
+
+            Response.Redirect(Globals.NavigateURL());
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            ViewState["TitleCount"] = TextBox.Text;
+            Page_Load(sender, e);
+        }
+
+        protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+        {
+            if (TextBox.Text != null)
+                TextBox.Text = string.Empty;
+            DateTime dt = Calendar1.SelectedDate;
+            ViewState["DateCountDay"] = dt;
+            Page_Load(sender, e);
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            ViewState["DateCount"] = DateTime.Now.AddDays(-1);
+            Calendar1.SelectedDates.Clear();
+            Page_Load(sender, e);
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            ViewState["DateCount"] = DateTime.Now.AddMonths(-1);
+            Calendar1.SelectedDates.Clear();
+            Page_Load(sender, e);
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            ViewState["DateCount"] = DateTime.Now.AddDays(-7);
+            Calendar1.SelectedDates.Clear();
+            Page_Load(sender, e);
+        }
+
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            ViewState["TitleCount"] = "";
+            Calendar1.SelectedDates.Clear();
+            Page_Load(sender, e);
+        }
+    }
+}
